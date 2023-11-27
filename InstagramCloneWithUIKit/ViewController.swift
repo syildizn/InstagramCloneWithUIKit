7bba8de992ec87e1fe208b7ad9cf78a467712cb2
Seçkin Yıldız <syildiznbjk@gmail.com>
2023-11-27 18:49:16 +0300

    Initial Commit

---
+++ InstagramCloneWithUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramCloneWithUIKit
+//
+//  Created by Seçkin Yıldız on 27.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
