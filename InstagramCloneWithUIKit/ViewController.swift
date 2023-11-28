796e046a04ef0b901d3ae19972ec7b50fc1bdabb
Seçkin Yıldız <syildiznbjk@gmail.com>
2023-11-28 19:33:11 +0300

    .

--- InstagramCloneWithUIKit/ViewController.swift
+++ InstagramCloneWithUIKit/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func SignIn(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    
+    @IBAction func SignUp(_ sender: Any) {
+    }
+    
+    
 }
 
