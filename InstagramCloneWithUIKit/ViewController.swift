9b2d1c321dd8e38acea9a1526a80825634a2b8d8
Seçkin Yıldız <syildiznbjk@gmail.com>
2023-11-30 15:04:43 +0300

    .

--- InstagramCloneWithUIKit/ViewController.swift
+++ InstagramCloneWithUIKit/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseCore
+
 
 
 class ViewController: UIViewController {
@@ -25,12 +28,66 @@ class ViewController: UIViewController {
 
   
     @IBAction func signIn(_ sender: Any) {
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+        if emailText.text != "" && passwordText.text != "" {
+            
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                /*if error != nil {
+                    self.makeAlert(titleInput: "Error !", messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }*/
+                
+                if let error = error as NSError? {
+                    print("Hata Kodu: \(error.code), Hata Mesajı: \(error.localizedDescription)")
+                        var message = ""
+
+                        switch error.code {
+                        case AuthErrorCode.invalidEmail.rawValue:
+                            message = "Geçersiz email adresi."
+                        default:
+                            message = "E-posta adresi veya şifre hatalı. Lütfen bilgilerinizi kontrol edin."
+                        }
+
+                        self.makeAlert(titleInput: "Hata!", messageInput: message)
+                    } else {
+                        self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                    }
+            }
+            
+        }else{
+            makeAlert(titleInput: "Error !", messageInput: "Email or password is empty")
+        }
+        
+        
         print("basıldı")
     }
     
     @IBAction func signUp(_ sender: Any) {
         
+        if emailText.text != "" && passwordText.text != "" {
+         
+            
+            Auth.auth().createUser(withEmail: emailText.text! , password: passwordText.text! ) { authdata, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error !", messageInput: error?.localizedDescription ?? "Error !")
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        }else{
+            makeAlert(titleInput: "Error !", messageInput: "Email or password is empty")
+        }
+        
+        
+    }
+    
+    func makeAlert(titleInput:String,messageInput:String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
